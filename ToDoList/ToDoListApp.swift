b34d1dde884b9000f8d64ee3ff3d6ac3059c5d82
Caio Castro <caio.castro3909@gmail.com>
2022-11-07 17:49:42 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Caio Castro on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
